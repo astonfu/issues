ebe2d589e029213d6ec0beeba8fb892fb922c74a
Hunter Foo <fudhao@gmail.com>
2017-10-03 22:40:50 +0800

    json body.

--- mix.exs
+++ mix.exs
@@ -23,7 +23,8 @@ defmodule Issues.Mixfile do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-      {:httpoison, "~> 0.13"}
+      {:httpoison, "~> 0.13"},
+      {:jsx, "~> 2.8.2"},
     ]
   end
 end
