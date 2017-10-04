10e1b219bd6e49fda795b680f668bf687209606e
Hunter Foo <fudhao@gmail.com>
2017-10-04 12:39:05 +0800

    add logger.

--- config/config.exs
+++ config/config.exs
@@ -11,6 +11,7 @@ use Mix.Config
 # You can configure your application as:
 #
 config :issues, github_url: "https://api.github.com"
+config :logger, compile_time_purge_level: :info
 #
 # and access this configuration in your application as:
 #
